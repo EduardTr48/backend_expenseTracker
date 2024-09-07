f1abce9e0e057825dc4a9db413baca3c636b1e18
tiago <tiago.edu.rodriguez@gmail.com>
2024-09-07 15:17:29 -0300

    update controlador de Expense

--- src/main/java/com/example/expense_tracker/controller/ExpenseController.java
+++ src/main/java/com/example/expense_tracker/controller/ExpenseController.java
@@ -29,66 +29,68 @@ public class ExpenseController {
         return ResponseEntity.ok(expenseDTOS);
     }
     @GetMapping("/{id}")
-    public ResponseEntity<ExpenseDTO> getExpenseById(@PathVariable Long id){
+    public ResponseEntity<?> getExpenseById(@PathVariable Long id){
         Optional<Expense> optionalExpense = expenseService.getExpenseById(id);
-        if(optionalExpense.isPresent()){
-            Expense expense = optionalExpense.get();
-            ExpenseDTO expenseDTO = ExpenseMapper.toDTO(expense);
-            return ResponseEntity.ok(expenseDTO);
-        }else{
-            return ResponseEntity.notFound().build();
+        if(optionalExpense.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("El gasto no fue encontrado");
         }
-
-
+        Expense expense = optionalExpense.get();
+        ExpenseDTO expenseDTO = ExpenseMapper.toDTO(expense);
+        return ResponseEntity.ok(expenseDTO);
     }
     @PostMapping
-    public ResponseEntity<ExpenseDTO> addExpense(@RequestBody ExpenseDTO expenseDTO){
+    public ResponseEntity<?> addExpense(@RequestBody ExpenseDTO expenseDTO){
         Optional<Category> optionalCategory = categoryService.getCategoryById(expenseDTO.getCategoryId());
-        if(optionalCategory.isPresent()){
-            Category category = optionalCategory.get();
-            Expense newExpense = ExpenseMapper.toEntity(expenseDTO, category);
-            expenseService.addExpense(newExpense);
-            ExpenseDTO responseDTO = ExpenseMapper.toDTO(newExpense);
-            return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
-        }else{
-            return ResponseEntity.notFound().build();
+        if(optionalCategory.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("La categoria no existe");
         }
+
+        Category category = optionalCategory.get();
+        Expense newExpense = ExpenseMapper.toEntity(expenseDTO, category);
+        expenseService.addExpense(newExpense);
+        ExpenseDTO responseDTO = ExpenseMapper.toDTO(newExpense);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ExpenseDTO> updateExpense(@PathVariable Long id, @RequestBody ExpenseDTO expenseDTO){
+    public ResponseEntity<?> updateExpense(@PathVariable Long id, @RequestBody ExpenseDTO expenseDTO){
         Optional<Expense> optionalExpense = expenseService.getExpenseById(id);
-        if(optionalExpense.isPresent()){
-            Expense expense = optionalExpense.get();
-            expense.setName(expenseDTO.getName());
-            expense.setAmount(expenseDTO.getAmount());
-            expense.setDate(expenseDTO.getDate());
 
-            Optional<Category> optionalCategory = categoryService.getCategoryById(expenseDTO.getCategoryId());
-
-            if(optionalCategory.isPresent()){
-                expense.setCategory(optionalCategory.get());
-            }
-
-            expenseService.updateExpense(id, expense);
-            ExpenseDTO responseDTO = ExpenseMapper.toDTO(expense);
-
-            return ResponseEntity.ok(responseDTO);
-        }else{
-            return ResponseEntity.notFound().build();
+        if(optionalExpense.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("El gasto no fue encontrado");
 
         }
 
+        Expense expense = optionalExpense.get();
+        expense.setName(expenseDTO.getName());
+        expense.setAmount(expenseDTO.getAmount());
+        expense.setDate(expenseDTO.getDate());
+
+        Optional<Category> optionalCategory = categoryService.getCategoryById(expenseDTO.getCategoryId());
+
+        if(optionalCategory.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("La categoria no existe");
+
+        }
+        expense.setCategory(optionalCategory.get());
+
+        expenseService.updateExpense(id, expense);
+        ExpenseDTO responseDTO = ExpenseMapper.toDTO(expense);
+
+        return ResponseEntity.ok(responseDTO);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteExpense(@PathVariable Long id){
-        if(expenseService.getExpenseById(id).isPresent()){
-            expenseService.deleteExpense(id);
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-        }else{
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    public ResponseEntity<String> deleteExpense(@PathVariable Long id){
+        Optional<Expense> optionalExpense = expenseService.getExpenseById(id);
+        if(optionalExpense.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("El gasto no existe");
         }
+        expenseService.deleteExpense(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body("El gasto fue eliminado existosamente");
+
     }
 
 
